1055f5cc5d48d5fbc623b6d28847a05ca7c5d8bb
kimzing <kimzing@163.com>
2020-06-09 18:06:41 +0800

    完成对springboot restful修改

---
+++ springboot-restful/src/main/java/com/kimzing/restful/service/UserService.java
@@ -0,0 +1,25 @@
+package com.kimzing.restful.service;
+
+import com.kimzing.base.utils.result.ApiResult;
+import com.kimzing.restful.domain.dto.UserDTO;
+import com.kimzing.restful.domain.dto.UserQueryDTO;
+
+/**
+ * 用户模拟服务.
+ *
+ * @author KimZing - kimzing@163.com
+ * @since 2019/12/28 16:16
+ */
+public interface UserService {
+
+    ApiResult save(UserDTO userDTO);
+
+    ApiResult remove(Long id);
+
+    ApiResult update(UserDTO userDTO);
+
+    ApiResult find(Long id);
+
+    ApiResult list(UserQueryDTO userQuery);
+
+}
